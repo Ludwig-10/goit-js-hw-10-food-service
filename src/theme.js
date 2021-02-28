d2e66ee97a875107581000011453efc43ba5fd7d
Denys Karakoi <denys.karakoi@gmail.com>
2021-02-28 19:37:54 +0100

    dz 10

--- src/theme.js
+++ src/theme.js
@@ -2,4 +2,31 @@ const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-  
+  const bodyRef = document.querySelector('body');
+  const switchRef = document.querySelector('#theme-switch-toggle');
+  const themeStorage = localStorage.getItem('theme');
+ 
+  isThemeStorage();
+
+  switchRef.addEventListener('change', themeChecked);
+
+function themeChecked() {
+  switchRef.checked ? darkTheme() : lightTheme();
+}
+
+function isThemeStorage() {
+  themeStorage === 'Theme.DARK' ? darkTheme() : lightTheme();
+  }
+
+function darkTheme() {
+  switchRef.checked = true;
+
+  bodyRef.classList.add(Theme.DARK);
+  bodyRef.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', 'Theme.DARK');
+}
+function lightTheme() {
+  bodyRef.classList.add(Theme.LIGHT);
+  bodyRef.classList.remove(Theme.DARK);
+  localStorage.setItem('theme', 'Theme.LIGHT');
+}
